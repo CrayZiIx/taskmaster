d440f13cc30a29d3b6094d7886c7f6d69acb0109
CrayZiIx <118490456+CrayZiIx@users.noreply.github.com>
2026-07-29 15:38:03 +0200

    refactor: clean the code, implement core process func

--- cmd/main.go
+++ cmd/main.go
@@ -4,7 +4,7 @@ import (
 	"fmt"
 
 	"github.com/CrayZiIx/taskmaster/internal/daemon/config"
-	"github.com/CrayZiIx/taskmaster/internal/daemon/monitoring"
+	"github.com/CrayZiIx/taskmaster/internal/daemon/process"
 )
 
 func main() {
@@ -15,25 +15,8 @@ func main() {
 		return
 	}
 
-	var processes []monitoring.Process
-
-	for name, program := range cfg.Programs {
-		fmt.Printf("loading config for process : %v\n", name)
-		p, err := monitoring.NewProcess(name)
-		p.Config = program
-		p.LoadConfig()
-		p.LoadEnv()
-		// to do: looks how to define those
-		// tmpCmd.Stdout = program.Output.StdOut
-		// tmpCmd.stderr = progam.Output.StdErr
-		if program.Journey.AutoStart {
-			err = p.ExecCommand()
-			if err != nil {
-				panic(err)
-			}
-			fmt.Printf("prog exited success: %v & exit code %d\n", p.Command.ProcessState.Success(), p.GetExitCode())
-		}
-		fmt.Printf("config loaded : %v\n", p)
-		processes = append(processes, *p)
-	}
+	p := process.New("ls", cfg.Programs["ls"])
+	p.Start()
+	p.Wait()
+	fmt.Println(p.ExitCode)
 }
---
+++ internal/daemon/process/process.go
@@ -0,0 +1,85 @@
+package process
+
+import (
+	"fmt"
+	"os"
+	"os/exec"
+
+	"github.com/CrayZiIx/taskmaster/internal/daemon/config"
+)
+
+type State string
+
+const (
+	NOT_STARTED   State = "not_started"
+	RUNNING       State = "running"
+	EXITED        State = "exited"
+	EXITED_WERROR State = "exited_with_error"
+)
+
+type Process struct {
+	Name     string
+	Conf     config.ConfigurationProgram
+	Cmd      *exec.Cmd
+	State    State
+	ExitCode int
+}
+
+func New(name string, conf config.ConfigurationProgram) *Process {
+	cmd := exec.Command(conf.Command[0], conf.Command[1:]...)
+	cmd.Dir = conf.Workdir
+
+	for key, value := range conf.Environment {
+		cmd.Env = append(cmd.Env, fmt.Sprintf("%s=%s", key, value))
+	}
+
+	if conf.Output.StdOut == "" {
+		cmd.Stdout = os.Stdout
+	}
+	if conf.Output.StdErr == "" {
+		cmd.Stderr = os.Stderr
+	}
+
+	return &Process{
+		Name:  name,
+		Conf:  conf,
+		Cmd:   cmd,
+		State: NOT_STARTED,
+	}
+}
+
+func (p *Process) Start() error {
+	err := p.Cmd.Start()
+	if err != nil {
+		return err
+	}
+	p.State = RUNNING
+	return nil
+}
+
+func (p *Process) Wait() error {
+	err := p.Cmd.Wait()
+	if err != nil {
+		return err
+	}
+	p.ExitCode = p.Cmd.ProcessState.ExitCode()
+	switch {
+	case p.ExitCode != 0:
+		p.State = EXITED_WERROR
+	default:
+		p.State = EXITED
+	}
+	return nil
+}
+
+func (p *Process) Kill() error {
+	err := p.Cmd.Process.Kill()
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+func (p *Process) Status() State {
+
+}
